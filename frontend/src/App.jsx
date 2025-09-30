92be1db9809d0ae063772719c70ae1f0a62d2c2b
Harshvardhan <shreeharshpb11@gmail.com>
2025-09-30 23:18:58 +0530

    added react router dom for routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
